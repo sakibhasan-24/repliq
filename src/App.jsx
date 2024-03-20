3e66e4dcbf9aef527ac1fd68dcdf5129f3d7345e
sakibhasan-24 <sakibhasan7724@gmail.com>
2024-03-20 23:20:28 +0600

    set up

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+export default function App() {
+  return <div>App</div>;
+}
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyByeiSO12zDUcxAyLGUx8QkfCrkHqdyNsw",
+  authDomain: "repliq-cf55c.firebaseapp.com",
+  projectId: "repliq-cf55c",
+  storageBucket: "repliq-cf55c.appspot.com",
+  messagingSenderId: "162434340928",
+  appId: "1:162434340928:web:ccec6765d4dff7c86b67a5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
